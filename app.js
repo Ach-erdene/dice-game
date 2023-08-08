5612b21dd93e4c8df6ea2908a29e849ba66637d9
Achka <aachka8@gmail.com>
2023-08-08 15:58:49 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -6,15 +6,23 @@ var score = [];
 //toglogchdiin eeljindee tsugluulj bgaa onoog hadgalah huvisagch
 var roundScore = 0;
 //shoonii ali talaaraa buusniig hadgalah huvisagch, 1-6 gesen utgiig ene huvisagchid sanamsarguigeer uusgej ugnuu
-var dice = Math.floor(Math.random() * 6 ) + 1;
+window.document.getElementById("score-0").textContent = '0';
 
-window.document.querySelector("#score-0").textContent = 0;
+window.document.getElementById("score-1").textContent = '0';
 
-window.document.querySelector("#score-1").textContent = 0;
+window.document.getElementById("current-0").textContent = '0';
 
-window.document.querySelector("#current-0").textContent = 0;
-window.document.querySelector("#current-1").textContent = 0;
+window.document.getElementById("current-1").textContent = '0';
 
-window.document.querySelector(".dice").style.display = 'none'
 
-console.log("Шоо: " + dice);
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = 'none'
+document.querySelector(".btn-roll").addEventListener('click', function (){
+      var diceNumber = Math.floor(Math.random() * 6 ) + 1;
+
+      diceDom.style.display = 'block';
+      diceDom.src = "dice-" + diceNumber + ".png";
+
+
+
+});
